e0eaa47194acf0e32dba44463091cc925dfd0e46
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2022-10-31 21:23:07 +0700

    Добавьте файлы проекта.

---
+++ TembroRecord/MainWindow.xaml.cs
@@ -0,0 +1,393 @@
+﻿using CSCore.CoreAudioAPI;
+using CSCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CSCore.SoundOut;
+using CSCore.SoundIn;
+using System.Diagnostics;
+using System.IO;
+using CSCore.Codecs;
+using CSCore.Codecs.WAV;
+using static System.Net.WebRequestMethods;
+using System.Threading;
+using File = System.IO.File;
+
+namespace TembroRecord
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        string langindex, fileDeleteRec1, fileDeleteCutRec1, fileDeleteRec2, fileDeleteCutRec2;
+        private int SampleRate, SoundClick;
+        string myfile;
+        string cutmyfile;
+
+        private WasapiOut mSoundOut;
+        private WasapiCapture mSoundIn;
+        private ISampleSource mMp3;
+        private SimpleMixer mMixer;
+        private IWaveSource mSource;
+        private MMDeviceCollection mOutputDevices;
+        private MMDeviceCollection mInputDevices;
+        private SampleDSPTurbo mDspTurbo;
+
+        private FileInfo fileInfo1 = new FileInfo("Data_Load.tmp");
+        private FileInfo FileLanguage = new FileInfo("Data_Language.tmp");
+        private FileInfo fileinfo = new FileInfo("DataTemp.tmp");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Mixer()
+        {
+            try
+            {
+
+                mMixer = new SimpleMixer(1, SampleRate) //стерео, 44,1 КГц
+                {
+                    //Right = true,
+                    //Left = true,
+                    FillWithZeros = true,
+                    DivideResult = true, //Для этого установлено значение true, чтобы избежать звуков тиков из-за превышения -1 и 1.
+                };
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в Mixer: \r\n" + ex.Message;
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in Mixer: \r\n" + ex.Message;
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+        }
+
+        private void Languages()
+        {
+            try
+            {
+                StreamReader FileLanguage = new StreamReader("Data_Language.tmp");
+                File.WriteAllText("Data_Load.tmp", "1");
+                File.WriteAllText("DataTemp.tmp", "0");
+                langindex = FileLanguage.ReadToEnd();
+                if(langindex == "0")
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в Languages: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in Languages: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+        }
+
+        private void SoundIn()
+        {
+            try
+            {
+                mSoundIn = new WasapiCapture(/*false, AudioClientShareMode.Exclusive, 1*/);
+                Dispatcher.Invoke(() => mSoundIn.Device = mInputDevices[cmbInput.SelectedIndex]);
+                mSoundIn.Initialize();
+                mSoundIn.Start();
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в SoundIn: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in SoundIn: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+        }
+
+        private void SoundOut()
+        {
+            try
+            {
+
+                mSoundOut = new WasapiOut(/*false, AudioClientShareMode.Exclusive, 1*/);
+                Dispatcher.Invoke(() => mSoundOut.Device = mOutputDevices[cmbOutput.SelectedIndex]);
+                //mSoundOut.Device = mOutputDevices[cmbOutput.SelectedIndex];
+
+
+                if (SoundClick == 0)
+                {
+                    mSoundOut.Initialize(mMp3.ToWaveSource(32).ToMono());
+                    mSoundOut.Volume = 5;
+                }
+                else
+                {
+                    mSoundOut.Initialize(mMixer.ToWaveSource(32).ToMono());
+                    mSoundOut.Volume = 15;
+                }
+
+
+                mSoundOut.Play();
+
+
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в SoundOut: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+
+                }
+                else
+                {
+                    string msg = "Error in SoundOut: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+
+                }
+            }
+        }
+
+        private async void Sound(string FileName)
+        {
+            try
+            {
+                SoundClick = 0;
+                Mixer();
+                mMp3 = CodecFactory.Instance.GetCodec(FileName).ToMono().ToSampleSource();
+                mMixer.AddSource(mMp3.ChangeSampleRate(mMp3.WaveFormat.SampleRate));
+                await Task.Run(() => SoundOut());
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в Sound: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in Sound: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+        }
+
+        private async void Recording1()
+        {
+            try
+            {
+                myfile = "MyRecord1.wav";
+                cutmyfile = "cutMyRecord1.wav";
+                fileDeleteRec1 = myfile;
+                fileDeleteCutRec1 = cutmyfile;
+                if (File.Exists(myfile))
+                {
+                    File.Delete(myfile);
+                }
+                if (File.Exists(cutmyfile))
+                {
+                    File.Delete(cutmyfile);
+                }
+                using (mSoundIn = new WasapiCapture())
+                {
+                    mSoundIn.Device = mInputDevices[cmbInput.SelectedIndex];
+                    mSoundIn.Initialize();
+
+                    mSoundIn.Start();
+                    pbRecord.Visibility = Visibility.Visible;
+                    lbRecordPB.Visibility = Visibility.Visible;
+                    using (WaveWriter record = new WaveWriter(cutmyfile, mSoundIn.WaveFormat))
+                    {
+                        mSoundIn.DataAvailable += (s, data) => record.Write(data.Data, data.Offset, data.ByteCount);
+                        for (int i = 0; i < 100; i++)
+                        {
+                            pbRecord.Value++;
+                            await Task.Delay(35);
+                            if (pbRecord.Value == 25)
+                            {
+                                string uri1 = @"ReSelf - Mental detox\Progressbar\Group 13.png";
+                                ImgPBRecordBack.ImageSource = new ImageSourceConverter().ConvertFromString(uri1) as ImageSource;
+                            }
+                            else if (pbRecord.Value == 50)
+                            {
+                                string uri2 = @"ReSelf - Mental detox\Progressbar\Group 12.png";
+                                ImgPBRecordBack.ImageSource = new ImageSourceConverter().ConvertFromString(uri2) as ImageSource;
+                            }
+                            else if (pbRecord.Value == 75)
+                            {
+                                string uri3 = @"ReSelf - Mental detox\Progressbar\Group 11.png";
+                                ImgPBRecordBack.ImageSource = new ImageSourceConverter().ConvertFromString(uri3) as ImageSource;
+                            }
+                            else if (pbRecord.Value == 95)
+                            {
+                                string uri4 = @"ReSelf - Mental detox\Progressbar\Group 10.png";
+                                ImgPBRecordBack.ImageSource = new ImageSourceConverter().ConvertFromString(uri4) as ImageSource;
+                            }
+                        }
+                        //Thread.Sleep(5000);
+
+                        mSoundIn.Stop();
+                        lbRecordPB.Visibility = Visibility.Hidden;
+                        pbRecord.Value = 0;
+                        pbRecord.Visibility = Visibility.Hidden;
+
+                    }
+                    Thread.Sleep(1000);
+                    string uri = @"ReSelf - Mental detox\Progressbar\progressbar-backgrnd.png";
+                    ImgPBRecordBack.ImageSource = new ImageSourceConverter().ConvertFromString(uri) as ImageSource;
+
+                }
+                if (langindex == "0")
+                {
+                    string msg = "Запись и обработка завершена. Сейчас появится графическое изображение вашего голоса.";
+                    LogClass.LogWrite(msg);
+                }
+                else
+                {
+                    string msg = "Recording and processing completed. A graphic representation of your voice will now appear.";
+                    LogClass.LogWrite(msg);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в Recording1: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in Recording1: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+        }
+
+        private void WinTembroRecord_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (SoftCl.IsSoftwareInstalled("Microsoft Visual C++ 2015-2022 Redistributable (x86) - 14.32.31332") == false)
+                {
+                    Process.Start("VC_redist.x86.exe");
+                }
+
+                MMDeviceEnumerator deviceEnum = new MMDeviceEnumerator();
+                mInputDevices = deviceEnum.EnumAudioEndpoints(DataFlow.Capture, DeviceState.Active);
+                MMDevice activeDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia);
+
+                SampleRate = activeDevice.DeviceFormat.SampleRate;
+
+                foreach (MMDevice device in mInputDevices)
+                {
+                    cmbInput.Items.Add(device.FriendlyName);
+                    if (device.DeviceID == activeDevice.DeviceID) cmbInput.SelectedIndex = cmbInput.Items.Count - 1;
+                }
+
+
+                //Находит устройства для вывода звука и заполняет комбобокс
+                activeDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                mOutputDevices = deviceEnum.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active);
+
+                foreach (MMDevice device in mOutputDevices)
+                {
+                    cmbOutput.Items.Add(device.FriendlyName);
+                    if (device.DeviceID == activeDevice.DeviceID) cmbOutput.SelectedIndex = cmbOutput.Items.Count - 1;
+                }
+
+                string[] filename = File.ReadAllLines(fileInfo1.FullName);
+                if (filename.Length == 1)
+                {
+                    Languages();
+                }
+                if (!File.Exists("log.tmp"))
+                {
+                    File.Create("log.tmp").Close();
+                }
+                else
+                {
+                    if (File.ReadAllLines("log.tmp").Length > 1000)
+                    {
+                        File.WriteAllText("log.tmp", " ");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (langindex == "0")
+                {
+                    string msg = "Ошибка в Loaded: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+                else
+                {
+                    string msg = "Error in Loaded: \r\n" + ex.Message;
+                    LogClass.LogWrite(msg);
+                    MessageBox.Show(msg);
+                    Debug.WriteLine(msg);
+                }
+            }
+        }
+
+
+    }
+}
